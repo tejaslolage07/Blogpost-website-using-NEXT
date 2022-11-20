4889daffc51e4c937e86295997fa04c133abb09a
Tejas Lolage <tejaslolage@gmail.com>
2022-11-20 22:24:29 +0530

    Implemented navbar for all pages, beautified the home page

---
+++ hunting-coder/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>This is my about component</div>
+  )
+}
+
+export default about
---
+++ hunting-coder/pages/blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>This is blog</div>
+  )
+}
+
+export default blog
