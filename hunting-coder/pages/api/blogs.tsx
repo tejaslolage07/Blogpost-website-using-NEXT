c28ba364267f8d60aba3ea820ef8a67e2a679f88
Tejas Lolage <tejaslolage@gmail.com>
2022-11-23 00:51:03 +0530

    Added an api calling system and reading the json blog files through the api

--- hunting-coder/pages/api/blogs.tsx
+++ hunting-coder/pages/api/blogs.tsx
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as fs from "fs"
 
 type Data = {
   name: string
@@ -9,5 +10,8 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Tejas Lolage' })
+  fs.readFile("blogdata/how-to-learn-javascript.json", "utf-8", (err, data)=>{
+    res.status(200).json(JSON.parse(data))
+  })
 }
+
