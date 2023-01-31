3a9e0fd79ef6831d0f97a48da5ab7c460f287b59
Tejas Lolage <tejaslolage@gmail.com>
2023-02-01 01:03:12 +0530

    Fetching blogpost data using /getblog endpoint

--- hunting-coder/pages/blogpost/[slug].js
+++ hunting-coder/pages/blogpost/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useRouter } from "next/router";
 import styles from "../../styles/BlogPost.module.css";
 
@@ -6,18 +6,26 @@ import styles from "../../styles/BlogPost.module.css";
 // 1. To find the file according to the Slug request.
 // 2. Populate them inside the page.
 const Slug = () => {
+    const [blog, setBlog] = useState();
     const router = useRouter();
-    const { Slug } = router.query;
+    useEffect(() => {
+        if(!router.isReady) return;
+        const { Slug } = router.query;
+        fetch(`http://localhost:3000/api/getblog?slug=${Slug}`).then((a) => {
+            return a.json();
+      })
+      .then((parsed) => {
+        setBlog(parsed);
+      });
+    }, [router.isReady]);
+
     return (
         <div>
             <main className={styles.main}>
-                <h1>The title of this page is {Slug}</h1>
+                <h1>{blog && blog.title}</h1>
                 <hr />
                 <div className={styles.blogItem}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-                    deleniti eveniet magni nulla sit accusantium laboriosam quaerat nemo
-                    beatae, officia consequatur! Possimus laudantium est dolorem
-                    architecto quibusdam quam sed excepturi nihil mollitia.
+                    {blog && blog.content}
                 </div>
             </main>
         </div>
